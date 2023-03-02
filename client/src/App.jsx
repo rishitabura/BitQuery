a070071d6bf70312454f438ecac960f9a4f201aa
Rishita <burarishita@gmail.com>
2023-03-02 19:27:50 +0530

    create campaign -> create profile in frontend on home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,7 @@ import { Sidebar, Navbar} from './components';
 import { CampaignDetails, CreateCampaign, Home, Profile} from './pages';
 const App = () => {
   return (
-    <div className="relative sm:-8 p-4 bg-[#13131a] 
-    min-h-screen flex flex-row">
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 
       relative">
         <Sidebar />
