a844e3f54dec225533477a5ad6e98ad91b3f91ce
Rohan <rohancrrm@gmail.com>
2023-03-03 21:47:03 +0530

    Changed the navbar and removed the search bar from navbar and added it to homepage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   const address = "0xabc";
 
   return (
-    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6 mt-5">
+    <div className="flex flex-col-reverse mb-[35px] gap-6 mt-5"> {/* removed md:flex-reverse to align the help and profile button to the right*/}
       {/* div for logo  */}
       {/* search bar */}
       {/* <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2  h-[52px] bg-[#1c1c24] rounded-[100px]">
@@ -37,7 +37,8 @@ const Navbar = () => {
       {/* </div> */}
 
       {/* tried to do something but ended up doing nothing */}
-      <div className=" lg:flex-1 bg-[#1c1c24] rounded-[100px] pb-5">
+      {/* Well the search bar isn't needed here as it is in the center*/}
+      {/* <div className=" lg:flex-1 bg-[#1c1c24] rounded-[100px] pb-5">
         <div>
           <input
             type="text"
@@ -45,10 +46,10 @@ const Navbar = () => {
             className="flex  w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"
           />
         </div>
-      </div>
+      </div> */}
 
       {/* CREATE PROFILE BUTTON FOR small devices*/}
-      <div className="sm:flex hidden flex-row justify-end gap-4S ">
+      <div className="sm:flex hidden flex-row justify-end gap-[10px] ">
         <CustomButton
           btnType="button"
           title={address ? "Create a profile" : "Connect"}
@@ -59,7 +60,6 @@ const Navbar = () => {
           }}
         />
 
-        {/* added help button for small devices*/}
         <CustomButton
           btnType="button"
           title={address ? "Help" : "Connect"}
@@ -81,6 +81,7 @@ const Navbar = () => {
         </Link>
       </div>
 
+      {/* added help button for small devices*/}
       <div className="sm:hidden flex justify-between items-center relative">
         <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
           <img
@@ -133,7 +134,8 @@ const Navbar = () => {
             ))}
           </ul>
 
-          {/* acreate profile for large devices */}
+          {/* create profile for small devices */}
+          {/* This should'nt actually be in the navbar but on the homepage where there would be a create profile button on the homepage itself*/}
           <div className="flex mx-4">
             <CustomButton
               btnType="button"
