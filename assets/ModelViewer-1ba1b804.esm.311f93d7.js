4e58e82f1b80be153152fb1f00f1b39e2c4c817f
Rishita <burarishita@gmail.com>
2023-08-21 01:37:12 +0530

    Updates

--- assets/ModelViewer-1ba1b804.esm.311f93d7.js
+++ assets/ModelViewer-1ba1b804.esm.311f93d7.js
@@ -1,4 +1,4 @@
-import{R as wm,j as uu}from"./index.d55147a3.js";/**
+import{R as wm,j as uu}from"./index.f44ea196.js";/**
  * @license
  * Copyright 2017 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
